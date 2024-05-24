ddfe44e02aca11768e9139d455ef4d8140d3a18d
Jawad <jawadnasiri2222@gmail.com>
2024-05-24 16:32:52 +0200

    the method get and post

---
+++ 01/showData.php
@@ -0,0 +1,9 @@
+<?php
+
+
+
+
+echo "First Name: " .$_POST["f-name"]."<br>";
+echo "Last Name: " .$_POST["l-name"]."<br>";
+echo "Email: " .$_POST["email"]."<br>";
+echo "Password: " .$_POST["pass"]."<br>";
