b60cb7d5d06c7faa6637f482d7ff45aa998def16
Jawad <jawadnasiri2222@gmail.com>
2024-05-27 13:47:26 +0200

    session & cookies

---
+++ 03/index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+
+// set a session 
+
+
+
+// session_start();
+
+// $_SESSION['name'] = "Jawad";
+
+
+
+
+
+
+// set a cookie 
+
+// echo time();
+
+// setCookie('name' , 'Jawad' , time() + 10);
---
+++ 03/showName.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+// set a session 
+
+// session_start();
+
+// echo $_SESSION['name'];
+
+
+
+
+
+
+
+
+// set a cookie 
+
+// echo $_COOKIE['name'];
